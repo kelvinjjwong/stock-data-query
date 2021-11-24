d009678cc67154b75e198853c7723cb0988fda0f
Kelvin Wong <kelvinjjwong@outlook.com>
2021-11-24 10:24:19 +0800

    add translation

--- link-to-menu.sh
+++ link-to-menu.sh
@@ -2,6 +2,9 @@
 
 target_folder=~/Library/Scripts/Stock-Data
 config_file=~/stock-data/config.yaml
+application_config=~/stock-data/application.yaml
+
+cp ./application.yaml ~/stock-data/
 
 if [[ ! -e $target_folder ]]; then
   echo "Open Script Editor -> Preference -> General -> Show Script menu in menu bar"
@@ -14,14 +17,26 @@ if [[ ! -e $config_file ]]; then
   echo "You can configure in $config_file"
 fi
 
-cp ./add-stock-code-by-select.sh $target_folder/
-cp ./add-stock-code-by-input.sh $target_folder/
-cp ./query-stocks-by-dividend-rate.sh $target_folder/
-cp ./query-selected-stocks-by-dividend-rate.sh $target_folder/
+langs=(`defaults read NSGlobalDomain AppleLanguages`);
+lang=`echo ${langs[1]/,/} | sed -e 's/\"//g'  | awk -F'-' '{print $1}'`
+if [[ "$lang" != "zh" ]]; then
+  lang=end
+fi
+
+FILE_ADD_STOCK_BY_INPUT=`yq e ".translation.script.add-stock-code-by-input.$lang" $application_config`
+FILE_ADD_STOCK_BY_SELECT=`yq e ".translation.script.add-stock-code-by-select.$lang" $application_config`
+FILE_QUERY_STOCKS_BY_DIVIDED_RATE=`yq e ".translation.script.query-stocks-by-dividend-rate.$lang" $application_config`
+FILE_QUERY_SELECTED_STOCKS_BY_DIVIDED_RATE=`yq e ".translation.script.query-selected-stocks-by-dividend-rate.$lang" $application_config`
+FILE_QUERY_DIVIDEND_HISTORY=`yq e ".translation.script.query-dividend-history.$lang" $application_config`
+
+cp ./add-stock-code-by-select.sh $target_folder/$FILE_ADD_STOCK_BY_SELECT
+cp ./add-stock-code-by-input.sh $target_folder/$FILE_ADD_STOCK_BY_INPUT
+cp ./query-stocks-by-dividend-rate.sh $target_folder/$FILE_QUERY_STOCKS_BY_DIVIDED_RATE
+cp ./query-selected-stocks-by-dividend-rate.sh $target_folder/$FILE_QUERY_SELECTED_STOCKS_BY_DIVIDED_RATE
 
 yq e '.query.dividend-history[]' $config_file | while read code;
 do
-  cp ./query-dividend-history.sh $target_folder/query-dividend-history.${code}.sh
+  cp ./query-dividend-history.sh $target_folder/$FILE_QUERY_DIVIDEND_HISTORY.${code}
 done
 
 echo "copied to $target_folder"
